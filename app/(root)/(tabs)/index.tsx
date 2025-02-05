af9d147bbfb6fa8dd4417f582420bbd443829496
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-05 14:16:05 +0530

    Added Tailwind configs

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router"
+import React from "react"
+import {Text , View} from "react-native"
+export default function Index(){
+    return (
+        <View style={{flex:1 , justifyContent:"center" , alignItems:"center"}}>
+           <Link href="/sign-in">Sign In</Link>
+           <Link href="/explore">Explore</Link>
+           <Link href="/profile">Profile</Link>
+           
+           <Link href="/properties/1">Properties</Link>
+        </View>
+    )
+}
