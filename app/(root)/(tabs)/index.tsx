eb6766b7e9ebdff1213c1106153a0baf9b045512
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-06 22:28:40 +0530

    Added first page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import {Text , View} from "react-native"
 export default function Index(){
     return (
         <View style={{flex:1 , justifyContent:"center" , alignItems:"center"}}>
-            <Text className="font-bold text-7xl flex flex-row">Welcome to restate</Text>
+            <Text className="font-bold text-3xl font-rubik-light">Welcome to restate</Text>
            <Link href="/sign-in">Sign In</Link>
            <Link href="/explore">Explore</Link>
            <Link href="/profile">Profile</Link>
