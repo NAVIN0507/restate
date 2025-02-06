91e239f0fd1cbd152e3dbe6ab2be1b0c48d5fea6
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-07 00:01:20 +0530

    Added Tabs and profile page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,11 +5,7 @@ export default function Index(){
     return (
         <View style={{flex:1 , justifyContent:"center" , alignItems:"center"}}>
             <Text className="font-bold text-3xl font-rubik-light">Welcome to restate</Text>
-           <Link href="/sign-in">Sign In</Link>
-           <Link href="/explore">Explore</Link>
-           <Link href="/profile">Profile</Link>
-           
-           <Link href="/properties/1">Properties</Link>
+      
         </View>
     )
 }
